d6e5539c000546f53d9070a78659c37112d6eb30
William J <william.jelvin@gmail.com>
2020-04-20 18:55:22 +0200

    getting coordinates from device

--- app/src/main/java/se/aniam/rakavagen/viewmodels/MainViewModel.java
+++ app/src/main/java/se/aniam/rakavagen/viewmodels/MainViewModel.java
@@ -1,18 +1,22 @@
 package se.aniam.rakavagen.viewmodels;
 
 import android.app.Application;
-import android.content.Context;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+
+import se.aniam.rakavagen.services.LocationService;
 
 
 public class MainViewModel extends AndroidViewModel {
 
     private String text =  "Hello world!!";
+    private LocationService locationService;
+
 
     public MainViewModel(@NonNull Application application) {
         super(application);
+        locationService = new LocationService(getApplication());
     }
 
 
@@ -22,6 +26,10 @@ public class MainViewModel extends AndroidViewModel {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public LocationService getLocationService() {
+        return this.locationService;
     }
 
 }
--- app/src/main/java/se/aniam/rakavagen/views/MainActivity.java
+++ app/src/main/java/se/aniam/rakavagen/views/MainActivity.java
@@ -1,8 +1,12 @@
 package se.aniam.rakavagen.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -11,6 +15,8 @@ import se.aniam.rakavagen.viewmodels.MainViewModel;
 import se.aniam.rakavagen.viewmodels.MainViewModelFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int PERMISSIONS_REQUEST_LOCATION_CODE = 123;
 
     private MainViewModel viewModel;
 
@@ -21,10 +27,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView mainText = findViewById(R.id.mainText);
         mainText.setText(viewModel.getText());
-//        if (savedInstanceState == null) {
-//            getSupportFragmentManager().beginTransaction()
-//                    .replace(R.id.container, ArrowFragment.newInstance())
-//                    .commitNow();
-//        }
+        checkForLocationPermission();
+        viewModel.getLocationService().observe(this, loc -> mainText.setText(loc.getLatitude() + " " + loc.getLongitude()));
     }
+
+    private void checkForLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted, so ask for permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_LOCATION_CODE);
+        }
+    }
+
 }
