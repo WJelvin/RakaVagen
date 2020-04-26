07fb36546c03a78cda651116be83d503469caab4
William J <william.jelvin@gmail.com>
2020-04-27 00:17:05 +0200

    added splash screen

--- app/src/main/java/se/aniam/rakavagen/views/MainActivity.java
+++ app/src/main/java/se/aniam/rakavagen/views/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
 
         // Get viewmodel from the viewmodel factory.
